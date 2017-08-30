fe4186bec883555fc659220f5ff9b3084992070a
Ilyas Ronef <ilyas@divandesign.biz>
2017-08-30 11:23:55 +0500

    * The following parameters were renamed (with backward compatibility): 	* “tpl” → “result_itemTpl”. 	* “toPlaceholder” → “result_toPlaceholder”. 	* “placeholderName” → “result_toPlaceholder_name”.

--- ddGetParentId.php
+++ ddGetParentId.php
@@ -11,11 +11,11 @@
  * 
  * @param $id {integer} — Document Id. Default: [*id*].
  * @param $level {integer} — Parent level (1 — the immediate parent; 2 — the parent of the immediate parent; -1 — the last parent; -2 — the parent before the last; etc). Default: 1.
- * @param $tpl {string_chunkName|string} — Template for output (chunk name or code via “@CODE:” prefix). Available placeholders: [+id+]. Default: '@CODE:[+id+]'.
+ * @param $result_itemTpl {string_chunkName|string} — Template for output (chunk name or code via “@CODE:” prefix). Available placeholders: [+id+]. Default: '@CODE:[+id+]'.
  * @param $result_itemsNumber {integer|'all'} — The number of parents that will be returned. Default: 1.
  * @param $result_itemsGlue {string} — The string that combines items while rendering. Default: ''.
- * @param $toPlaceholder {0|1} — Returns value to the placeholder. Default: 0.
- * @param $placeholderName {string} — Placeholder name. Default: 'ddParent'.
+ * @param $result_toPlaceholder {0|1} — Returns value to the placeholder. Default: 0.
+ * @param $result_toPlaceholder_name {string} — Placeholder name. Default: 'ddParent'.
  * 
  * @link http://code.divandesign.biz/modx/ddgetparentid/1.1
  * 
@@ -26,11 +26,18 @@ $result = '';
 
 require_once $modx->getConfig('base_path').'assets/libs/ddTools/modx.ddtools.class.php';
 
+//Bacward compatibility
+extract(ddTools::verifyRenamedParams($params, [
+	'result_itemTpl' => 'tpl',
+	'result_toPlaceholder' => 'toPlaceholder',
+	'result_toPlaceholder_name' => 'placeholderName',
+]));
+
 $id = isset($id) ? $id : $modx->documentIdentifier;
-$tpl = isset($tpl) ? $modx->getTpl($tpl) : '[+id+]';
+$result_itemTpl = isset($result_itemTpl) ? $modx->getTpl($result_itemTpl) : '[+id+]';
 $result_itemsGlue = isset($result_itemsGlue) ? $result_itemsGlue : '';
-$toPlaceholder = (isset($toPlaceholder) && $toPlaceholder == '1') ? true : false;
-$placeholderName = isset($placeholderName) ? $placeholderName : 'ddParent';
+$result_toPlaceholder = (isset($result_toPlaceholder) && $result_toPlaceholder == '1') ? true : false;
+$result_toPlaceholder_name = isset($result_toPlaceholder_name) ? $result_toPlaceholder_name : 'ddParent';
 
 if (!isset($level)){
 	$level = 1;
@@ -69,7 +76,7 @@ if ($parents_len > 0){
 
 foreach ($parents as $parentIndex => $parentId){
 	$parents[$parentIndex] = ddTools::parseText([
-		'text' => $tpl,
+		'text' => $result_itemTpl,
 		'data' => ['id' => $parentId]
 	]);
 }
@@ -77,8 +84,8 @@ foreach ($parents as $parentIndex => $parentId){
 $result = implode($result_itemsGlue, $parents);
 
 //Если надо, выводим в плэйсхолдер, или просто возвращаем
-if ($toPlaceholder){
-	$modx->setPlaceholder($placeholderName, $result);
+if ($result_toPlaceholder){
+	$modx->setPlaceholder($result_toPlaceholder_name, $result);
 	$result = '';
 }
 
