23e984a328483e726033f5c235892de958e77502
Ilyas Ronef <ilyas@divandesign.biz>
2017-08-30 11:14:55 +0500

    + Added an ability to return several parents (see the “result_itemsNumber” and “result_itemsGlue” parameters).

--- ddGetParentId.php
+++ ddGetParentId.php
@@ -11,7 +11,9 @@
  * 
  * @param $id {integer} — Document Id. Default: [*id*].
  * @param $level {integer} — Parent level (1 — the immediate parent; 2 — the parent of the immediate parent; -1 — the last parent; -2 — the parent before the last; etc). Default: 1.
- * @param $tpl {string_chunkName|string} — Template for output (chunk name or code via “@CODE:” prefix). Available placeholders: [+id+]. Default: —.
+ * @param $tpl {string_chunkName|string} — Template for output (chunk name or code via “@CODE:” prefix). Available placeholders: [+id+]. Default: '@CODE:[+id+]'.
+ * @param $result_itemsNumber {integer|'all'} — The number of parents that will be returned. Default: 1.
+ * @param $result_itemsGlue {string} — The string that combines items while rendering. Default: ''.
  * @param $toPlaceholder {0|1} — Returns value to the placeholder. Default: 0.
  * @param $placeholderName {string} — Placeholder name. Default: 'ddParent'.
  * 
@@ -25,6 +27,8 @@ $result = '';
 require_once $modx->getConfig('base_path').'assets/libs/ddTools/modx.ddtools.class.php';
 
 $id = isset($id) ? $id : $modx->documentIdentifier;
+$tpl = isset($tpl) ? $modx->getTpl($tpl) : '[+id+]';
+$result_itemsGlue = isset($result_itemsGlue) ? $result_itemsGlue : '';
 $toPlaceholder = (isset($toPlaceholder) && $toPlaceholder == '1') ? true : false;
 $placeholderName = isset($placeholderName) ? $placeholderName : 'ddParent';
 
@@ -49,21 +53,28 @@ if ($parents_len > 0){
 	if ($level > $parents_len){$level = -1;}
 	//Если уровень задаётся от начала (не от конца), то его надо бы декриминировать (т.к. самого себя в массиве $parents не будет)
 	if ($level > 0){$level--;}
+	//Количество возвращаемых родителей
+	if ($result_itemsNumber == 'all'){
+		$result_itemsNumber = $parent_len;
+	}else{
+		$result_itemsNumber = intval($parent_len);
+	}
 	
 	//Получаем необходимого родителя
-	$parents = array_slice($parents, $level, 1);
-	$result = $parents[0];
+	$parents = array_slice($parents, $level, $result_itemsNumber);
+	$parents = array_reverse($parents);
 }else{
-	$result = $id;
+	$parents = [$id];
 }
 
-//Если задан шаблон, выводим по шаблону
-if (isset($tpl)){
-	$result = ddTools::parseText([
-		'text' => $modx->getTpl($tpl),
-		'data' => ['id' => $result]
+foreach ($parents as $parentIndex => $parentId){
+	$parents[$parentIndex] = ddTools::parseText([
+		'text' => $tpl,
+		'data' => ['id' => $parentId]
 	]);
 }
+
+$result = implode($result_itemsGlue, $parents);
 
 //Если надо, выводим в плэйсхолдер, или просто возвращаем
 if ($toPlaceholder){
