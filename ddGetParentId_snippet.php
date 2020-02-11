624e2c6b397a32d4f4e556fd24c996ab3618ef47
Ilyas Ronef <ilyas@DivanDesign.ru>
2020-02-11 21:18:08 +0300

    * Minor changes.

--- ddGetParentId_snippet.php
+++ ddGetParentId_snippet.php
@@ -11,7 +11,7 @@
  * 
  * @param $id {integer} — Document Id. Default: [*id*].
  * @param $level {integer} — Parent level (1 — the immediate parent; 2 — the parent of the immediate parent; -1 — the last parent; -2 — the parent before the last; etc). Default: 1.
- * @param $result_itemTpl {string_chunkName|string} — Template for output (chunk name or code via “@CODE:” prefix). Available placeholders: [+id+]. Default: '@CODE:[+id+]'.
+ * @param $result_itemTpl {stringChunkName|string} — Template for output (chunk name or code via “@CODE:” prefix). Available placeholders: [+id+]. Default: '@CODE:[+id+]'.
  * @param $result_itemsNumber {integer|'all'} — The number of parents that will be returned. Default: 1.
  * @param $result_itemsGlue {string} — The string that combines items while rendering. Default: ''.
  * @param $result_toPlaceholder {0|1} — Returns value to the placeholder. Default: 0.
@@ -25,7 +25,10 @@
 $result = '';
 
 //Include (MODX)EvolutionCMS.libraries.ddTools
-require_once $modx->getConfig('base_path').'assets/libs/ddTools/modx.ddtools.class.php';
+require_once(
+	$modx->getConfig('base_path') .
+	'assets/libs/ddTools/modx.ddtools.class.php'
+);
 
 //Bacward compatibility
 extract(ddTools::verifyRenamedParams(
@@ -37,14 +40,34 @@ extract(ddTools::verifyRenamedParams(
 	]
 ));
 
-$id = isset($id) ? $id : $modx->documentIdentifier;
-$result_itemTpl = isset($result_itemTpl) ? $modx->getTpl($result_itemTpl) : '[+id+]';
-$result_itemsGlue = isset($result_itemsGlue) ? $result_itemsGlue : '';
-$result_toPlaceholder = (
-	isset($result_toPlaceholder) &&
-	$result_toPlaceholder == '1'
-) ? true : false;
-$result_toPlaceholder_name = isset($result_toPlaceholder_name) ? $result_toPlaceholder_name : 'ddParent';
+$id =
+	isset($id) ?
+	$id :
+	$modx->documentIdentifier
+;
+$result_itemTpl =
+	isset($result_itemTpl) ?
+	$modx->getTpl($result_itemTpl) :
+	'[+id+]'
+;
+$result_itemsGlue =
+	isset($result_itemsGlue) ?
+	$result_itemsGlue :
+	''
+;
+$result_toPlaceholder =
+	(
+		isset($result_toPlaceholder) &&
+		$result_toPlaceholder == '1'
+	) ?
+	true :
+	false
+;
+$result_toPlaceholder_name =
+	isset($result_toPlaceholder_name) ?
+	$result_toPlaceholder_name :
+	'ddParent'
+;
 
 if (!isset($level)){
 	$level = 1;
@@ -53,7 +76,7 @@ if (!isset($level)){
 	if ($level == 'ultimate'){
 		$level = -1;
 	}else{
-		$level = (int) $level;
+		$level = intval($level);
 	}
 }
 
@@ -64,15 +87,22 @@ $parents_len = count($parents);
 //Если родители вообще есть
 if ($parents_len > 0){
 	//Если уровень задали больше, чем в принципе есть родителей, считаем, что нужен последний
-	if ($level > $parents_len){$level = -1;}
+	if ($level > $parents_len){
+		$level = -1;
+	}
 	//Если уровень задаётся от начала (не от конца), то его надо бы декриминировать (т.к. самого себя в массиве $parents не будет)
-	if ($level > 0){$level--;}
+	if ($level > 0){
+		$level--;
+	}
 	//Количество возвращаемых родителей
-	if ($result_itemsNumber == 'all'){ //Все родители
+	if ($result_itemsNumber == 'all'){
+		//Все родители
 		$result_itemsNumber = $parents_len;
 	}else if (isset($result_itemsNumber)){
+		//Заданное количество
 		$result_itemsNumber = intval($result_itemsNumber);
 	}else{
+		//Непосредственный
 		$result_itemsNumber = 1;
 	}
 	
@@ -89,7 +119,8 @@ if ($parents_len > 0){
 
 foreach (
 	$parents as
-	$parentIndex => $parentId
+	$parentIndex =>
+	$parentId
 ){
 	$parents[$parentIndex] = ddTools::parseText([
 		'text' => $result_itemTpl,
